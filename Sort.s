af6e7c0e6ff3ccb7a3fbd47033d61df87087fe79
NoRyyZ <39616554+NoRyyZ@users.noreply.github.com>
2019-02-27 15:52:34 +0100

    Imported Kleins Code

--- Sort.s
+++ Sort.s
@@ -11,10 +11,63 @@
 @ and controlling the motors accordingly.					   @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+@ Constants for assembler
+@ The following are defined in /usr/include/asm-generic/fcntl.h:
+@ Note that the values are specified in octal.
+        .equ      O_RDWR,00000002             @ open for read/write
+        .equ      O_DSYNC,00010000            @ synchronize virtual memory
+        .equ      __O_SYNC,04000000           @      programming changes with
+        .equ      O_SYNC,__O_SYNC|O_DSYNC     @ I/O memory
+@ The following are defined in /usr/include/asm-generic/mman-common.h:
+        .equ      PROT_READ,0x1               @ page can be read
+        .equ      PROT_WRITE,0x2              @ page can be written
+        .equ      MAP_SHARED,0x01             @ share changes
+@ The following are defined by me:
+@        .equ      PERIPH,0x3f000000           @ RPi 2 & 3 peripherals
+        .equ      PERIPH,0x20000000           @ RPi zero & 1 peripherals
+        .equ      GPIO_OFFSET,0x200000        @ start of GPIO device
+        .equ      TIMERIR_OFFSET,0xB000       @ start f´of IR and timer
+        .equ      O_FLAGS,O_RDWR|O_SYNC       @ open file flags
+        .equ      PROT_RDWR,PROT_READ|PROT_WRITE
+        .equ      NO_PREF,0
+        .equ      PAGE_SIZE,4096              @ Raspbian memory page
+        .equ      FILE_DESCRP_ARG,0           @ file descriptor
+        .equ      DEVICE_ARG,4                @ device address
+        .equ      STACK_ARGS,8                @ sp already 8-byte aligned
+
+TMPREG  .req      r5
+RETREG  .req      r6
+WAITREG .req      r8
+RLDREG  .req      r9
+GPIOREG .req      r10
+COLREG  .req      r11
+
+
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @ - START OF DATA SECTION @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-	.data
+		.data
+gpiomem:
+        .asciz    "/dev/gpiomem"
+mem:
+        .asciz    "/dev/mem"
+fdMsg:
+        .asciz    "File descriptor = %i\n"
+memMsgGpio:
+        .asciz    "(GPIO) Using memory at %p\n"
+memMsgTimerIR:
+        .asciz    "(Timer + IR) Using memory at %p\n"
+
+        .balign   4
+gpio_mmap_adr:
+        .word     0               @ ...
+gpio_mmap_fd:
+        .word     0
+timerir_mmap_adr:
+        .word     0
+timerir_mmap_fd:
+        .word     0
+
 @ - END OF DATA SECTION @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
@@ -22,8 +75,140 @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @ - START OF TEXT SECTION @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	.text
-	.balign 4
-	.global _start
+	@ externals for making use of std-functions
+        .extern printf
+
+        .balign   4
+        .global   main
+        .type     main, %function
+
+
+@ -----------------------------------------------------------------------------
+@ main entry point of the application
+@   param:     none
+@   return:    none
+@ -----------------------------------------------------------------------------
+main:
+        ldr r0, =IntroMsg
+        bl  printf
+
+        @ GET GPIO VIRTUAL MEMORY ---------------------------------------------
+        @ create backup and reserve stack space
+        sub       sp, sp, #16                 @ space for saving regs
+        str       r4, [sp, #0]                @ save r4
+        str       r5, [sp, #4]                @      r5
+        str       fp, [sp, #8]                @      fp
+        str       lr, [sp, #12]               @      lr
+        add       fp, sp, #12                 @ set our frame pointer
+        sub       sp, sp, #STACK_ARGS         @ sp on 8-byte boundary
+
+        @ open /dev/gpiomem for read/write and syncing
+        ldr       r0, =gpiomem                 @ address of /dev/gpiomem
+        ldr       r1, openMode                @ flags for accessing device
+        bl        open
+        mov       r4, r0                      @ use r4 for file descriptor
+
+        @ display file descriptor
+        ldr       r0, =fdMsg                  @ format for printf
+        mov       r1, r4                      @ file descriptor
+        bl        printf
+
+        @ map the GPIO registers to a virtual memory location so we can access them
+        str       r4, [sp, #FILE_DESCRP_ARG]  @ /dev/gpiomem file descriptor
+        ldr       r0, gpio                    @ address of GPIO
+        str       r0, [sp, #DEVICE_ARG]       @ location of GPIO
+        mov       r0, #NO_PREF                @ let kernel pick memory
+        mov       r1, #PAGE_SIZE              @ get 1 page of memory
+        mov       r2, #PROT_RDWR              @ read/write this memory
+        mov       r3, #MAP_SHARED             @ share with other processes
+        bl        mmap
+
+        @ save virtual memory address
+        ldr       r1, =gpio_mmap_adr          @ store gpio mmap (virtual address)
+        str       r0, [r1]
+        ldr       r1, =gpio_mmap_fd           @ store the file descriptor
+        str       r4, [r1]
+
+        ldr       r6, [r1]
+        mov       r1, r0                      @ display virtual address
+        ldr       r0, =memMsgGpio
+        bl        printf
+        mov       r1, r6
+        ldr       r0, =memMsgGpio
+        bl        printf
+
+        @ restore sp and free stack
+        add       sp, sp, #STACK_ARGS         @ fix sp
+        ldr       r4, [sp, #0]                @ restore r4
+        ldr       r5, [sp, #4]                @      r5
+        ldr       fp, [sp, #8]                @         fp
+        ldr       lr, [sp, #12]               @         lr
+        add       sp, sp, #16                 @ restore sp
+
+        @ GET TIMER + IR VIRTUAL MEMORY ---------------------------------------
+        @ create backup and reserve stack space
+        sub       sp, sp, #16                 @ space for saving regs
+        str       r4, [sp, #0]                @ save r4
+        str       r5, [sp, #4]                @      r5
+        str       fp, [sp, #8]                @      fp
+        str       lr, [sp, #12]               @      lr
+        add       fp, sp, #12                 @ set our frame pointer
+        sub       sp, sp, #STACK_ARGS         @ sp on 8-byte boundary
+
+        @ open /dev/gpiomem for read/write and syncing
+        ldr       r0, =mem                    @ address of /dev/mem
+        ldr       r1, openMode                @ flags for accessing device
+        bl        open
+        mov       r4, r0                      @ use r4 for file descriptor
+
+        @ display file descriptor
+        ldr       r0, =fdMsg                  @ format for printf
+        mov       r1, r4                      @ file descriptor
+        bl        printf
+
+        @ map the GPIO registers to a virtual memory location so we can access them
+        str       r4, [sp, #FILE_DESCRP_ARG]  @ /dev/mem file descriptor
+        ldr       r0, timerIR                 @ address of timer + IR
+        str       r0, [sp, #DEVICE_ARG]       @ location of timer +IR
+        mov       r0, #NO_PREF                @ let kernel pick memory
+        mov       r1, #PAGE_SIZE              @ get 1 page of memory
+        mov       r2, #PROT_RDWR              @ read/write this memory
+        mov       r3, #MAP_SHARED             @ share with other processes
+        bl        mmap
+
+        @ save virtual memory address
+        ldr       r1, =timerir_mmap_adr       @ store timer + IR mmap (virtual address)
+        str       r0, [r1]
+        ldr       r1, =timerir_mmap_fd        @ store the file descriptor
+        str       r4, [r1]
+
+        ldr       r6, [r1]
+        mov       r1, r0                      @ display virtual address
+        ldr       r0, =memMsgTimerIR
+        bl        printf
+        mov       r1, r6
+        ldr       r0, =memMsgTimerIR
+        bl        printf
+
+        @ restore sp and free stack
+        add       sp, sp, #STACK_ARGS         @ fix sp
+        ldr       r4, [sp, #0]                @ restore r4
+        ldr       r5, [sp, #4]                @      r5
+        ldr       fp, [sp, #8]                @         fp
+        ldr       lr, [sp, #12]               @         lr
+        add       sp, sp, #16                 @ restore sp
+
+        @ initialize all other hardware
+        b         hw_init
+
+
+@ --------------------------------------------------------------------
+@ Inititalize all needed GPIOs (see above), Set Input/Output mode, 
+@ Set starting values of some Pins (11, 17)
+@  param: none
+@  return: none
+@ -------------------------------------------------------------------
+hw_init:
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @	Table of all needed GPIOs for init in _start:
@@ -80,16 +265,6 @@
 @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-
-@ --------------------------------------------------------------------
-@ Inititalize all needed GPIOs (see above), Set Input/Output mode, 
-@ Set starting values of some Pins (11, 17)
-@  param: none
-@  return: none
-@ -------------------------------------------------------------------
-_start:
-
-
 @ --------------------------------------------------------------------
 @ Move Outlet Motor to starting position. The Hall sensor only returns 
 @ true/false, so in order to find the center the motor turns until true
@@ -112,7 +287,21 @@ startpos_cw:
 @  param: none
 @  return: none
 @ --------------------------------------------------------------------
-main:
+
+@ --------------------------------------------------------------------------------------------------------------------
+@
+@ ADDRESSES: Further definitions.
+@
+@ --------------------------------------------------------------------------------------------------------------------
+        .balign   4
+@ addresses of messages
+openMode:
+        .word     O_FLAGS
+gpio:
+        .word     PERIPH+GPIO_OFFSET
+timerIR:
+        .word     PERIPH+TIMERIR_OFFSET
+
 
 @ - END OF TEXT SECTION   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
